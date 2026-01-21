d1de5c20d3369233ec2ab6ad989fc8a165bb8e40
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-21 23:09:38 +0530

    Updated neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,7 +9,41 @@ return {
     },
     config = function()
       require('neo-tree').setup({
-        reveal=true,
+        window = {
+          mappings = {
+            ['e'] = function() vim.api.nvim_exec('Neotree focus filesystem left', true) end,
+            ['b'] = function() vim.api.nvim_exec('Neotree focus buffers left', true) end,
+            ['g'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
+          },
+        },
+        default_component_configs = {
+          indent = {
+            with_expanders = true,
+            expander_collapsed = "",
+            expander_expanded = "",
+            expander_highlight = "NeoTreeExpander",
+          },
+        },
+        event_handlers = {
+          {
+            event = "file_open_requested",
+            handler = function()
+              require("neo-tree.command").execute({ action = "close" })
+            end
+          },
+          {
+            event = "neo_tree_buffer_enter",
+            handler = function()
+              vim.cmd("highlight! Cursor blend=100")
+            end,
+          },
+          {
+            event = "neo_tree_buffer_leave",
+            handler = function()
+              vim.cmd("highlight! Cursor guibg=#5f87af blend=0")
+            end,
+          },
+        },
       })
       vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
       vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal float<CR>', {})
