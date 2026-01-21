34a599fac18c8f1ce59f64dfaaee78b66623eb4a
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-22 02:51:40 +0530

    Added git integration to the config

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,57 @@
+return {
+	{ "akinsho/git-conflict.nvim", version = "*", config = true },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gitsigns = require("gitsigns")
+
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					-- Navigation
+					map("n", "]c", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "]c", bang = true })
+						else
+							gitsigns.nav_hunk("next")
+						end
+					end)
+
+					map("n", "[c", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "[c", bang = true })
+						else
+							gitsigns.nav_hunk("prev")
+						end
+					end)
+
+					-- Actions
+					map("n", "<leader>hs", gitsigns.stage_hunk)
+					map("n", "<leader>hr", gitsigns.reset_hunk)
+
+					map("v", "<leader>hs", function()
+						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+
+					map("v", "<leader>hr", function()
+						gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+
+					map("n", "<leader>hS", gitsigns.stage_buffer)
+					map("n", "<leader>hR", gitsigns.reset_buffer)
+
+					map("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
+					map("n", "<leader>gd", ":Gitsigns diffthis<CR>")
+					map("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>")
+					map("o", "ih", ":Gitsigns select_hunk<CR>")
+					map("x", "ih", ":Gitsigns select_hunk<CR>")
+				end,
+			})
+		end,
+	},
+}
