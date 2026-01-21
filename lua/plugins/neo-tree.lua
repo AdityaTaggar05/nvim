7b0d0dac5ca231cf77030477cc8abf3a4f3fdb60
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-21 15:42:37 +0530

    Added neo-tree config

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    config = function()
+      require('neo-tree').setup({
+        reveal=true,
+      })
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
+      vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal float<CR>', {})
+    end,
+  }
+}
