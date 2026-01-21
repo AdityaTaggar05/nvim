8cfe3aada0d8a0b7b89cda829a8acc377a4d7002
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-22 01:22:36 +0530

    Updated lsp and mason configs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,32 +1,32 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {}
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      vim.diagnostic.config({
-        virtual_text = true,
-      })
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-        callback = function(_)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-          vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-          vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end
-      })
-    end
-  }
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "stylua" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.diagnostic.config({
+				virtual_text = true,
+			})
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(_)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+					vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+					vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+				end,
+			})
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,6 @@ return {
     config = function()
       local config = require("nvim-treesitter.config")
       config.setup({
-        ensure_installed = {"c", "cpp", "lua", "vim", "go", "python", "dart"},
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
