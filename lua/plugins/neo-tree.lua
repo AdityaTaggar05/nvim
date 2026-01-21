83ae66e906dde2ee9d6a3c4dc3752b047522c4e1
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-22 04:17:13 +0530

    Added .gitignore

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,41 +1,47 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    config = function()
-      require('neo-tree').setup({
-        window = {
-          mappings = {
-            ['e'] = function() vim.api.nvim_exec('Neotree focus filesystem left', true) end,
-            ['bb'] = function() vim.api.nvim_exec('Neotree focus buffers left', true) end,
-            ['gg'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
-          },
-        },
-        default_component_configs = {
-          indent = {
-            with_expanders = true,
-            expander_collapsed = "",
-            expander_expanded = "",
-            expander_highlight = "NeoTreeExpander",
-          },
-        },
-        event_handlers = {
-          {
-            event = "file_open_requested",
-            handler = function()
-              require("neo-tree.command").execute({ action = "close" })
-            end
-          },
-        },
-      })
-      vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
-      vim.keymap.set('n', '<leader>gf', ':Neotree git_status float<CR>', {})
-      vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal float<CR>', {})
-    end,
-  }
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		config = function()
+			require("neo-tree").setup({
+				window = {
+					mappings = {
+						["e"] = function()
+							vim.api.nvim_exec("Neotree focus filesystem left", true)
+						end,
+						["bb"] = function()
+							vim.api.nvim_exec("Neotree focus buffers left", true)
+						end,
+						["gg"] = function()
+							vim.api.nvim_exec("Neotree focus git_status left", true)
+						end,
+					},
+				},
+				default_component_configs = {
+					indent = {
+						with_expanders = true,
+						expander_collapsed = "",
+						expander_expanded = "",
+						expander_highlight = "NeoTreeExpander",
+					},
+				},
+				event_handlers = {
+					{
+						event = "file_open_requested",
+						handler = function()
+							require("neo-tree.command").execute({ action = "close" })
+						end,
+					},
+				},
+			})
+			vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+			vim.keymap.set("n", "<leader>gf", ":Neotree git_status float<CR>", {})
+			vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+		end,
+	},
 }
