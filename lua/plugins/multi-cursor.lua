ea233c7302bea85b9c618faa8d091ad872342061
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-22 03:32:45 +0530

    Fixed multi cursor support

---
+++ lua/plugins/multi-cursor.lua
@@ -0,0 +1,11 @@
+return {
+	"mg979/vim-visual-multi",
+	init = function()
+		vim.g.VM_default_mappings = -1
+		vim.g.VM_maps = {
+			["Find Under"] = "<C-d>",
+			["Find Subword Under"] = "<C-d>",
+			["Select All"] = "<A-n>",
+		}
+	end,
+}
