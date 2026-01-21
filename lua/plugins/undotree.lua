705353e68350d954059924030de9eedf55fcd283
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-22 04:13:09 +0530

    Added undotree config

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,10 @@
+return {
+	"mbbill/undotree",
+	config = function()
+    -- Only Windows
+		vim.g.undotree_DiffCommand = "FC"
+
+		vim.cmd("set undodir=undodir")
+		vim.keymap.set("n", "<leader>u", function() vim.cmd.UndotreeToggle() vim.cmd.UndotreeFocus() end)
+	end,
+}
