587d1f5f2e3f76615f1f25a07358d0d350beff50
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-21 23:37:16 +0530

    Changed the conflicting mapping for git status

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -13,7 +13,7 @@ return {
           mappings = {
             ['e'] = function() vim.api.nvim_exec('Neotree focus filesystem left', true) end,
             ['b'] = function() vim.api.nvim_exec('Neotree focus buffers left', true) end,
-            ['g'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
+            ['gs'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
           },
         },
         default_component_configs = {
