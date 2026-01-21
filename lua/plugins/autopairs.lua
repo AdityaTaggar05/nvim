228041ad4ad06171a5884b8b8525a773b53cf52d
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-22 03:54:43 +0530

    Introduced Fly Mode to the config

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,19 @@
 return {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	opts = {
-		fast_wrap = {},
+  -- For the legendary Fly Mode
+	{
+		"jiangmiao/auto-pairs",
+		event = "InsertEnter",
+		init = function()
+			vim.g.AutoPairsFlyMode = 1
+			vim.g.AutoPairsShortcutFastWrap = ""
+		end,
+	},
+  -- For the beautiful Fast Wrap implementation
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {
+			fast_wrap = {},
+		},
 	},
 }
