019131a5ae496ead4f493b4241d36db38b15c1b1
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-22 17:08:26 +0530

    Added cmp for file path completion

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -74,6 +74,15 @@ return {
 				}),
 			})
 
+			-- Use buffer source for `/` and `?`
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- Use cmdline & path source for ':'
 			cmp.setup.cmdline(":", {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = cmp.config.sources({
