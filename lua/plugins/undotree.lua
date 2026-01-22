5ec04d08688abf79f75277004fff1c4f3edca33c
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-22 13:28:18 +0530

    Added format on save config to none-ls

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,10 +1,13 @@
 return {
 	"mbbill/undotree",
 	config = function()
-    -- Only Windows
+		-- Only Windows
 		vim.g.undotree_DiffCommand = "FC"
 
 		vim.cmd("set undodir=undodir")
-		vim.keymap.set("n", "<leader>u", function() vim.cmd.UndotreeToggle() vim.cmd.UndotreeFocus() end)
+		vim.keymap.set("n", "<leader>u", function()
+			vim.cmd.UndotreeToggle()
+			vim.cmd.UndotreeFocus()
+		end)
 	end,
 }
