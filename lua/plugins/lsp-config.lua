8f97df60b18b2b983ff6d29feafe99ee757c6945
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-22 16:55:43 +0530

    Setup lua_ls and clangd lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,6 +15,11 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			vim.lsp.config("lua_ls", { capabilities = capabilities })
+			vim.lsp.config("clangd", { capabilities = capabilities })
+
 			vim.diagnostic.config({
 				virtual_text = true,
 			})
