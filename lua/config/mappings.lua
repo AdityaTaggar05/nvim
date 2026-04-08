2b429e6203e3f7d17243266d95a2a49a185789b9
Aditya Taggar <aditya05.mgg@gmail.com>
2026-04-08 17:16:00 +0530

    improve keybinds for pane split

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -33,8 +33,8 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<leader>v", ":vsp<CR>", { silent = true })
-vim.keymap.set("n", "<leader>s", ":sp<CR>", { silent = true })
+vim.keymap.set("n", "|", ":vsp<CR>", { silent = true })
+vim.keymap.set("n", "-", ":sp<CR>", { silent = true })
 
 -- Better Replace Commands
 vim.keymap.set("n", "<leader>R", function()
