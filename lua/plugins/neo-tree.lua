2f2277d2cb09301f05edba9ee57586ed4422cd2e
Aditya Taggar <aditya05.mgg@gmail.com>
2026-04-09 17:13:19 +0530

    enable silent commands for neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -59,7 +59,7 @@ return {
 					},
 				},
 			})
-			vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+			vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", { silent = true })
 			vim.keymap.set("n", "<leader>gf", ":Neotree git_status float<CR>", {})
 		end,
 	},
